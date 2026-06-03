6897ea4502f185bf5db60a9e7afb3db2860a9698
kimkim-IN <kimberly.barnabe@escola.pr.gov.br>
2026-06-03 13:53:21 +0000

    v1

--- animal.js
+++ animal.js
@@ -1,1 +1,10 @@
+const animal = {
+    nome: "Sakura", //string
+    especie: "Cachorro",
+    idade: 10, //number
+    tutor: false,
+    vacinada: true
+};
 
+console.log(animal);
+console.log(animal.nome);
