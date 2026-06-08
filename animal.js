f17125f266b49001a79e5cdb1208b30210787a92
kimkim-IN <kimberly.barnabe@escola.pr.gov.br>
2026-06-08 13:49:57 +0000

    v1

--- animal.js
+++ animal.js
@@ -1,10 +1,16 @@
 const animal = {
     nome: "Sakura", //string
     especie: "Cachorro",
-    idade: 10, //number
+    idade: 9, //number
     tutor: false,
     vacinada: true
 };
 
-console.log(animal);
+//console.log(animal);
 console.log(animal.nome);
+console.log(animal.tutor);
+
+animal.idade = 9;
+//console.log(animal);
+
+console.log(`${animal.nome} é um ${animal.especie} com ${animal.idade} anos de idade e possui sua vacina em dia (${animal.vacinada}).`);
