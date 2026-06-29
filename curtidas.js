4f3ec6e12676e817df383383c28b041a85823d3f
kimkim-IN <kimberly.barnabe@escola.pr.gov.br>
2026-06-29 13:55:36 +0000

    v1

--- curtidas.js
+++ curtidas.js
@@ -2,10 +2,11 @@ const feedJSON = require('./feed.json');
 
 //adicionar uma curtida ao post pelo id
 function curtir(feed, id) {
-    const post = feed[0].id === id ? feed[0]
+    const post = feed[0].id === id ? feed[0] : //Alterar a linha 5 inserindo:
+                feed[1].id === id ? feed[1] : feed[2]; //busca pelo id
 }
 
-if (!post) {
+if(!post) {
     console.log("Post não encontrado.");
     return;
 }
@@ -17,3 +18,18 @@ if(!post.publicado) {
 
 post.metricas.curtidas++;
 console.log(`Post de @${post.autor} agora tm ${post.metricas.curtidas} curtidas.`);
+
+//Remove uma curtida - não deixa ir abaixo de zero
+function descurtir(feed, id) {
+    const post = feed[0].id === id ? feed[0] :
+                feed[1].id === id ? feed[1] : feed[2];
+}
+
+if(post.metricas.curtidas === 0) {
+    console.log("Este post já está com 0 curtidas.");
+    return;
+}
+
+post.metricas.curtidas++;
+console.log(`💔 Curtida removida. Total ${post.metricas.curtidas}`);
+curtir(feedJSON, 1);
